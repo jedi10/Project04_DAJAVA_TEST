80a6d884fc03cea168cd58229d5b62f99cdc3ace
jedi10 <kyle10_dev@hotmail.fr>
2020-05-02 15:24:24 +0200

    TDD: Tests for less 30 minutes Parking Time
    price should be free for Car and Bike if Parking Time is under 30 minutes

--- parkingsystem/src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -8,6 +8,8 @@ import com.parkit.parkingsystem.service.FareCalculatorService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -122,6 +124,36 @@ public class FareCalculatorServiceTest {
         ticket.setParkingSpot(parkingSpot);
         fareCalculatorService.calculateFare(ticket);
         assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
+    }
+
+    @ParameterizedTest(name = "For {0} Minute(s) Car Parking Time, Price should be 0")
+    @ValueSource(ints = { 1, 15, 29 })
+    public void calculateFareCarWithLessThan30MinParkingTime(int arg){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (arg * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals(ticket.getPrice(), 0);
+    }
+
+    @ParameterizedTest(name = "For {0} Minute(s) Bike Parking Time, Price should be 0")
+    @ValueSource(ints = { 1, 15, 29 })
+    public void calculateFareBikeWithLessThan30MinParkingTime(int arg){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (arg * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals(ticket.getPrice(), 0);
     }
 
 }
